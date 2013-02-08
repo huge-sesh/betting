0e6a4d693589a2f34ea5639ce25cd6131741bd8a
sesh <sesh>
2013-02-08 18:04:19 -0500

    didn't add anything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bets::Application.config.secret_token = '9465a01bdcc41df3bd050253fe1c006eead8dd036f9065842834d53c49b1cc24b2c9629dd21969e54f49aa3f3958dfdcda7e9052d47182fffd2a524775a38def'
